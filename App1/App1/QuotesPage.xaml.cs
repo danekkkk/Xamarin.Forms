456f49d2b3004f188c02f1e1a474c7364c195285
danekkkk <101827298+danekkkk@users.noreply.github.com>
2023-10-29 20:02:04 +0100

    Update QuotesPage.xaml.cs

--- App1/App1/QuotesPage.xaml.cs
+++ App1/App1/QuotesPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,15 +9,29 @@ using Xamarin.Forms.Xaml;
 
 namespace App1
 {
-    // XAML compilation.
+    // Właczenie kompilacje XAML.
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class QuotesPage : ContentPage
     {
-        public QuotesPage()
+        // tablica z cytatami
+        public string[] Quotes =
+        {
+            "Jeśli potrafisz o czymś marzyć, to potrafisz tego dokonać.",
+            "Móc czegoś nie chcieć, to już jest szczeście.",
+            "Każdą pracę wykonuj, jakby miała ona być ostatnią w życiu.",
+            "Szczęście to jedyna rzecz, która się mnoży, jeśli się ją dzieli.",
+            "Nie wydawaj wyroku, zanim nie wysłuchasz przeciwnej strony"
+        };
+
+        // zmienna przechowujaca, ktory index tablicy wyswietlic
+        public int QuoteNum = 0;
+
+        public MainPage()
         {
             InitializeComponent();
-            BindingContext = this;
+            quoteLabel.Text = Quotes[QuoteNum];
 
+            // zmiana Paddingu w zaleznosci od platformy
             if (Device.OS == TargetPlatform.Android) Padding = new Thickness(20, 30, 20, 20);
             if (Device.OS == TargetPlatform.WinPhone) Padding = new Thickness(20, 40, 20, 20);
 
@@ -26,13 +40,16 @@ namespace App1
                 Android: new Thickness(20, 30, 20, 20),
                 WinPhone: new Thickness(20, 40, 20, 20)
             );
-
         }
 
-        public string[] Quotes { get; set; } = {
-            "Jeśli potrafisz o czymś marzyć, to potrafisz tego dokonać.",
-            "Móc czegoś nie chcieć, to już jest szczeście.",
-            "Każdą pracę wykonuj, jakby miała ona być ostatnią w życiu."
-        };
+        public void ChangeQuote(object sender, EventArgs e)
+        {
+            QuoteNum++;
+
+            if (QuoteNum > Quotes.Length - 1) QuoteNum = 0;
+
+            quoteLabel.Text = Quotes[QuoteNum];
+
+        }
     }
 }
